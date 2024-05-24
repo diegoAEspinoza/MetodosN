e848fab4abcab7f466ebac491a326d28e4922c94
Sterben22 <sterben97061@gmail.com>
2024-05-24 02:00:35 -0500

    Trabajen sus propios codigos

--- metodos/n_r.m
+++ metodos/n_r.m
@@ -1,14 +1,26 @@
 %Newton-RApshon
-function n_r(err)
+function  n_r(err)
   f = input('Ingrese la función f(x):','s');
-  a = input('ingrese extremo izquierdo:');
-  b = input('ingrese extremo derecho:');
+  a = input('Ingrese extremo izquierdo:');
+  b = input('Ingrese extremo derecho:');
   df = diff(sym(f));
-  lx(1) = (a+b)/2;
-  i=1;
-  while aprox <err;
-    x = i; fx = eval(f); dfx = eval(df);
-    lx(i+1) = lx(i)- (fx/dfx);
-    aprox = lx(i+1)-lx(i);
+  lx(1) = (a + b) / 2;
+
+  aprox = 0;
+
+  i = 1;
+  fprintf('Iter \t x_n \t\t err\n');
+  fprintf('%d \t %.9f \t %.9e\n', i-1, lx(i), aprox);
+  while aprox > err
+    x = lx(i);
+    fx = eval(f);
+    dfx = eval(df);
+    lx(i + 1) = lx(i) - (fx / dfx);
+    aprox = abs(lx(i + 1) - lx(i));
+    fprintf('%d \t %.9f \t %.9e\n', i, lx(i + 1), aprox);
+    i = i + 1;
   endwhile
+
+  fprintf('EL numero buscado es %d \n', lx(length(lx)));
+endfunction
 
--- metodos/secante.m
+++ metodos/secante.m
@@ -1,2 +1,38 @@
-function secante()
+function t = secante()
+  f_s = input('Ingrese la función f(x):','s');
+  a = input('Ingrese extremo izquierdo:');
+  b = input('Ingrese extremo derecho:');
+  tol = input('Ingrese la tolerancia: ');
+
+  aprox = inf; % Inicializar aprox con un valor grande para que entre en el bucle
+  f = sym(f_s);
+  df = diff(f);
+
+  x0 = (a + b) / 2;
+  x = x0;
+  f0 = eval(f);
+  df0 = eval(df);
+
+  x1 = x0 - (f0 / df0);
+  x = x1;
+  f1 = eval(f);
+
+  lx  = [x0, x1];
+  fx  = [f0, f1];
+  err = [0, abs(x0 - x1)];
+  i = 1;
+  fprintf('n \t x_n \t f_n \t err\n');
+  while abs(aprox) > tol % Corregido el criterio de convergencia
+    lx(i + 2)  = (lx(i) * fx(i + 1) - lx(i + 1) * fx(i)) / (fx(i + 1) - fx(i));
+
+    x = lx(i + 2);
+    fx(i + 2) = eval(f);
+
+    aprox = lx(i + 2) - lx(i + 1); % Calcula la aproximación actual
+    err(i + 2) = abs(aprox); % Guarda el valor absoluto de la aproximación actual
+    fprintf('%d \t %.6f \t %.6f \t %.6f\n', i, lx(i + 2), fx(i + 2), err(i + 2));
+    i += 1;
+  endwhile
+  t = [[0:(length(lx) - 1)]' lx' fx' err'];
 endfunction
+
